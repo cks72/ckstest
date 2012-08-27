a70ce55f7d6fabb61b62af53fa1270f950b9867a
cks72 <github@csb.e-plaza.de>
2012-08-27 14:49:20 +0200

    EntryPoint

---
+++ src/ckstest/client/CKStest.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package ckstest.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * @author csb_wac
+ *
+ */
+public class CKStest implements EntryPoint {
+
+	/**
+	 * 
+	 */
+	public CKStest() {
+		super();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.google.gwt.core.client.EntryPoint#onModuleLoad()
+	 */
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
